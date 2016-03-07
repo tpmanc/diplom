925f838cfeb7d1073ca61439e0b5a9bfce7238c7
tpmanc <tpxtrime@mail.ru>
2016-03-07 12:19:40 +0300

    tz

--- src/main/java/controllers/AdminIndexController.java
+++ src/main/java/controllers/AdminIndexController.java
@@ -4,17 +4,32 @@ import db.Database2;
 import models.CategoryModel;
 import models.FileModel;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @Controller
 public class AdminIndexController {
 
     @RequestMapping(value = { "/admin" }, method = RequestMethod.GET)
-    public String index(@RequestParam(value="name", required=false) String name, Model model) {
+    public String index(@RequestParam(value="name", required=false) String name, Model model, Principal principal) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        System.out.println(currentPrincipalName);
+        Object s = authentication.getDetails();
+
+        System.out.println(principal.getName());
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println("User has authorities: " + userDetails.getAuthorities());
+
         model.addAttribute("pageTitle", "Index");
 
         int fileCount = FileModel.getCount();
