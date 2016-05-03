dc470276a26ec2643a5ba289d35bda2150d0dffa
tpmanc <tpxtrime@mail.ru>
2016-05-03 21:26:09 +0300

    add log4j

--- src/main/java/config/StartupListener.java
+++ src/main/java/config/StartupListener.java
@@ -1,17 +1,31 @@
 package config;
 
-import helpers.ConfigDB;
+import org.apache.log4j.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.web.context.ContextLoader;
 
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Листенер события старта приложения
  */
 public class StartupListener implements ApplicationListener<ContextRefreshedEvent> {
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        ApplicationContext contextParent = contextRefreshedEvent.getApplicationContext().getParent();
+        if (contextParent != null) {
+            Properties props = new Properties();
+            try {
+                FileInputStream configStream = new FileInputStream("D:/log4j.properties");
+                props.load(configStream);
+                configStream.close();
+            } catch(IOException e) {
+                System.out.println("Error: Cannot laod configuration file ");
+            }
+            LogManager.resetConfiguration();
+            PropertyConfigurator.configure(props);
+        }
     }
 }
--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -6,6 +6,7 @@ import exceptions.InternalException;
 import exceptions.NotFoundException;
 import helpers.UserHelper;
 import models.SettingsModel;
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,19 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.ContextLoader;
 
-import java.sql.SQLException;
-
 
 /**
  * Контроллер входа на сайт
  */
 @Controller
 public class LoginController {
+    private static final Logger logger = Logger.getLogger(LoginController.class);
+
     @RequestMapping("/login")
     public String login(@RequestParam(value="error", required=false, defaultValue = "false") boolean error, Model model) {
         if (UserHelper.isLogin()) {
             return "redirect:/";
         }
+
+        logger.info("This is an info log entry");
 
         // если не заполнены настройки, то редиректим на страницу с настройками
         ApplicationContext ctx = ContextLoader.getCurrentWebApplicationContext();
